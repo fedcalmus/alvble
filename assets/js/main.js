a2c6b5b6df3aca07f2724e446a6ce9312ee0025f
fedcalmus <fedcalmus@gmail.com>
2018-05-03 18:23:36 +0400

    fonts and gen styles

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    var AppPlugins = {
+
+        /* Date picker */
+        datepicker: function () {
+            $('[data-toggle="datepicker"]').datepicker();
+        }
+    };
+
+    /* ------ Plugins Init ------ */
+    AppPlugins.datepicker();
+
+});
+
