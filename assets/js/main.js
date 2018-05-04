b60768dec0a227dbfb9594f17be1f1229b8df298
fedcalmus <fedcalmus@gmail.com>
2018-05-04 18:16:24 +0400

    done with footer and header

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,26 @@ $(function() {
         /* Date picker */
         datepicker: function () {
             $('[data-toggle="datepicker"]').datepicker();
+        },
+
+        /* Owl carousel */
+        owlCarousel: function () {
+            $('.owl-carousel').owlCarousel({
+                nav: false,
+                navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+                items: 1,
+                loop: true,
+                center: true,
+                margin: 0,
+                lazyLoad: true,
+                dots: true,
+            });
         }
     };
 
     /* ------ Plugins Init ------ */
     AppPlugins.datepicker();
+    AppPlugins.owlCarousel();
 
 });
 
